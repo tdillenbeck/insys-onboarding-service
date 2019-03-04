1daa4cafe8b714678dd573cef70a9281ed05b8b2
Zach Toolson <zach.toolson@getweave.com>
2019-03-04 08:43:39 -0700

    Write test to verify task instance create behavior works when on onboarder has partial links setup

--- internal/psql/taskinstance_test.go
+++ internal/psql/taskinstance_test.go
@@ -173,18 +173,44 @@ func TestTaskInstanceService_CreateFromTasks(t *testing.T) {
 	if err != nil {
 		t.Fatalf("could not create onboarder")
 	}
+	partialOnboarder, err := onboarderService.CreateOrUpdate(
+		context.Background(),
+		&app.Onboarder{
+			UserID:                       uuid.NewV4(),
+			ScheduleCustomizationLink:    null.NewString("schedule customization link"),
+			SchedulePortingLink:          null.NewString("schedule porting link"),
+			ScheduleNetworkLink:          null.NewString("schedule network link"),
+			ScheduleSoftwareInstallLink:  null.NewString(""),
+			SchedulePhoneInstallLink:     null.NewString(""),
+			ScheduleSoftwareTrainingLink: null.NewString("schedule software training link"),
+			SchedulePhoneTrainingLink:    null.NewString("schedule phone training link"),
+		},
+	)
+	if err != nil {
+		t.Fatalf("could not create onboarder")
+	}
 
 	assignedOnboarderLocationID := uuid.NewV4()
 	unassignedOnboarderLocationID := uuid.NewV4()
+	partialOnboarderLocationID := uuid.NewV4()
 
 	// assign an onboarder to a location
-	onbl := &app.OnboardersLocation{
-		OnboarderID: onb.ID,
-		LocationID:  assignedOnboarderLocationID,
-	}
-	onbl, err = onboardersLocationService.CreateOrUpdate(context.Background(), onbl)
+	_, err = onboardersLocationService.CreateOrUpdate(
+		context.Background(),
+		&app.OnboardersLocation{
+			OnboarderID: onb.ID,
+			LocationID:  assignedOnboarderLocationID,
+		},
+	)
 	if err != nil {
 		t.Fatalf("could not assign onboarder to a location")
+	}
+	_, err = onboardersLocationService.CreateOrUpdate(context.Background(), &app.OnboardersLocation{
+		OnboarderID: partialOnboarder.ID,
+		LocationID:  partialOnboarderLocationID,
+	})
+	if err != nil {
+		t.Fatalf("could not assign partial onboarder to a location")
 	}
 
 	type fields struct {
@@ -607,6 +633,221 @@ func TestTaskInstanceService_CreateFromTasks(t *testing.T) {
 				},
 				app.TaskInstance{
 					LocationID: assignedOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("d0da53a9-fbdb-4d22-85c6-ed521f237349")),
+					TaskID:     mustUUID(uuid.Parse("9aec502b-f8b8-4f10-9748-1fe4050eacde")),
+
+					ButtonContent:     null.NewString("Let's Start"),
+					ButtonExternalURL: null.NewString("schedule porting link"),
+					ButtonInternalURL: null.NewString("/porting"),
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"> <span class="insys-content-bold"> What is this?  </span> In order to port your phone numbers from your current provider to your new Weave phones, we need three things: <ul> <li>Info about your current phone account</li> <li>Your approval of the terms of service</li> <li>A copy of your current phone bill</li> </ul> </div> <div class="insys-content-body"> <span class="insys-content-bold">How long will this take?</span> 5-10 minutes </div>`,
+					DisplayOrder:      4,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Provide current phone account info",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:   "create links with an onboarder that has partial links setup",
+			fields: fields{DB: db},
+			args:   args{ctx: context.Background(), locationID: partialOnboarderLocationID},
+			want: []app.TaskInstance{
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("26ba2237-c452-42dd-95ca-a5e59dd2853b")),
+					TaskID:     mustUUID(uuid.Parse("16a6dc91-ec6b-4b09-b591-a5b0dfa92932")),
+
+					ButtonContent:     null.NewString("Schedule Call"),
+					ButtonExternalURL: null.NewString("https://getweave.chilipiper.com/book/installation-scheduler?type=software-installation"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> On this scheduled call, we will help get your patient or customer database syncing with Weave. This will allow you to start using many of Weave's software features.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 30-60 minutes</div><div class="insys-content-body"><span class="insys-content-bold">Anything else?</span> We may need to access your office server - so make sure you have access to that computer and the right login information.</div>`,
+					DisplayOrder:      2,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Sync your patient data to Weave",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("26ba2237-c452-42dd-95ca-a5e59dd2853b")),
+					TaskID:     mustUUID(uuid.Parse("720af494-38a4-499f-8633-9c8d5169cd43")),
+
+					ButtonContent:     null.NewString("Install Weave"),
+					ButtonExternalURL: null.NewString("http://www.weavehelp.com/software-install"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> A quick installation of Weave on other workstations in your office. Installing Weave on more workstations will help you make the most of features like Team Chat and two-way texting.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 5 minutes per workstation</div><div class="insys-content-body"><span class="insys-content-bold">Anything else?</span> Use the link below to download Weave throughout your office - if you need help, just let us know!</div>`,
+					DisplayOrder:      5,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Install Weave on other workstations in your office",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("26ba2237-c452-42dd-95ca-a5e59dd2853b")),
+					TaskID:     mustUUID(uuid.Parse("c20b65d8-e281-4e62-98f0-4aebf83e0bee")),
+
+					ButtonContent:     null.NewString("Watch Videos"),
+					ButtonExternalURL: null.NewString("schedule software training link"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> An online webinar - available to schedule or on demand - that will guide you through all the key features you can be using now, like two-way texting, automated appointment reminders, Team Chat, and much more.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 30 minutes</div>`,
+					DisplayOrder:      6,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Watch our helpful software training videos",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("26ba2237-c452-42dd-95ca-a5e59dd2853b")),
+					TaskID:     mustUUID(uuid.Parse("1120842a-a24b-40e8-b29e-0e05e89af99f")),
+
+					ButtonContent:     null.NewString("Learn How"),
+					ButtonExternalURL: null.NewString("http://www.weavehelp.com/weave-mobile-app/"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> A quick installation of our mobile app on your phone. Our mobile app is a great tool to see your schedule, field office calls, and chat with your team - all on-the-go, whenever you need access.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 3-5 minutes</div>`,
+					DisplayOrder:      7,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Install the Weave mobile app",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("ebc72a11-f1b3-40d5-888e-5b6aba66e871")),
+					TaskID:     mustUUID(uuid.Parse("7b15e061-8002-4edc-9bf4-f38c6eec6364")),
+
+					ButtonContent:     null.NewString("Schedule Call"),
+					ButtonExternalURL: null.NewString("schedule network link"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> A scheduled call during which a Weave technician will remotely access your workstation to check your office network and make recommendations to have the best experience with Weave.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 15 minutes</div>`,
+					DisplayOrder:      3,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Check your office network to ensure compatibility",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("ebc72a11-f1b3-40d5-888e-5b6aba66e871")),
+					TaskID:     mustUUID(uuid.Parse("fd4f656c-c9f1-47b8-96ad-3080b999a843")),
+
+					ButtonContent:     null.NewString("Schedule Call"),
+					ButtonExternalURL: null.NewString("https://getweave.chilipiper.com/book/installation-scheduler?type=phone-installation"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> A scheduled call during which a Weave technician will work with you (or your IT professional) to guide you through physically connecting the new phones to internet and power.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 60 minutes</div><div class="insys-content-body"><span class="insys-content-bold">Anything else?</span> Typically, we will want to help you connect the new phones side-by-side with the old phones while we work with your current phone company for a few days to transition your phones service to Weave.</div>`,
+					DisplayOrder:      8,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Install your new phones",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("ebc72a11-f1b3-40d5-888e-5b6aba66e871")),
+					TaskID:     mustUUID(uuid.Parse("47743fae-c775-45d5-8a51-dc7e3371dfa4")),
+
+					ButtonContent:     null.NewString("Watch Videos"),
+					ButtonExternalURL: null.NewString("schedule phone training link"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> An online webinar - available to schedule or on demand - that will guide you through all the key features of your new phone system, including voicemail and auto-attendant setup, transferring calls, placing callers on hold, and much more.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 30 minutes</div>`,
+					DisplayOrder:      9,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Watch our helpful phone training videos",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
+					CategoryID: mustUUID(uuid.Parse("ebc72a11-f1b3-40d5-888e-5b6aba66e871")),
+					TaskID:     mustUUID(uuid.Parse("2d2df285-9211-48fc-a057-74f7dee2d9a4")),
+
+					ButtonContent:     null.NewString("Schedule Call"),
+					ButtonExternalURL: null.NewString("schedule customization link"),
+					ButtonInternalURL: null.String{},
+					CompletedAt:       null.Time{},
+					CompletedBy:       null.String{},
+					VerifiedAt:        null.Time{},
+					VerifiedBy:        null.String{},
+					Content:           `<div class="insys-content-body"><span class="insys-content-bold">What is this?</span> A scheduled call during which your onboarding agent will help customize various features of your phone system - like which phones ring on inbound calls, the name and extension of each phone, and any advanced call routing or auto attendant.</div><div class="insys-content-body"><span class="insys-content-bold">How long will this take?</span> 30 minutes</div>`,
+					DisplayOrder:      10,
+					Status:            0,
+					StatusUpdatedAt:   time.Now(),
+					StatusUpdatedBy:   null.NewString("Weave - default"),
+					Title:             "Customize your phone system",
+					Explanation:       null.String{},
+
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				},
+				app.TaskInstance{
+					LocationID: partialOnboarderLocationID,
 					CategoryID: mustUUID(uuid.Parse("d0da53a9-fbdb-4d22-85c6-ed521f237349")),
 					TaskID:     mustUUID(uuid.Parse("9aec502b-f8b8-4f10-9748-1fe4050eacde")),
 
