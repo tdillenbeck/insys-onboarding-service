0bd341780298ae1751a202267eb6c2c7bf5f661f
Zach Toolson <zach.toolson@getweave.com>
2019-07-09 14:36:06 -0600

    Write test to update existing onboarder

--- internal/psql/onboarder_test.go
+++ internal/psql/onboarder_test.go
@@ -17,6 +17,17 @@ func TestOnboarderService_CreateOrUpdate(t *testing.T) {
 	db := initDBConnection(t, psqlConnString)
 
 	userID := uuid.NewV4()
+	id := uuid.NewV4()
+	existingUserID := uuid.NewV4()
+
+	//create existing onoarder to test update functionality
+	query := `INSERT INTO insys_onboarding.onboarders
+		(id, user_id, schedule_customization_link)
+		VALUES ($1, $2, 'testing exsinting_schedule_customization_link')`
+	_, err := db.ExecContext(context.Background(), query, id, existingUserID)
+	if err != nil {
+		t.Fatalf("could not create onboarder: %v\n", err)
+	}
 
 	type fields struct {
 		DB *wsql.PG
@@ -62,6 +73,36 @@ func TestOnboarderService_CreateOrUpdate(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name:   "successfully updates an existing onboarder",
+			fields: fields{DB: db},
+			args: args{
+				ctx: context.Background(),
+				onb: &app.Onboarder{
+					SalesforceUserID:             null.NewString("testing salesforce user id"),
+					ScheduleCustomizationLink:    null.NewString("testing schedule customization link"),
+					ScheduleNetworkLink:          null.NewString("testing schedule network link"),
+					SchedulePhoneInstallLink:     null.NewString("testing schedule phone install link"),
+					SchedulePhoneTrainingLink:    null.NewString("testing schedule phone training link"),
+					SchedulePortingLink:          null.NewString("testing schedule porting link"),
+					ScheduleSoftwareInstallLink:  null.NewString("testing software install link"),
+					ScheduleSoftwareTrainingLink: null.NewString("testing software training link"),
+					UserID:                       existingUserID,
+				},
+			},
+			want: &app.Onboarder{
+				SalesforceUserID:             null.NewString("testing salesforce user id"),
+				ScheduleCustomizationLink:    null.NewString("testing schedule customization link"),
+				ScheduleNetworkLink:          null.NewString("testing schedule network link"),
+				SchedulePhoneInstallLink:     null.NewString("testing schedule phone install link"),
+				SchedulePhoneTrainingLink:    null.NewString("testing schedule phone training link"),
+				SchedulePortingLink:          null.NewString("testing schedule porting link"),
+				ScheduleSoftwareInstallLink:  null.NewString("testing software install link"),
+				ScheduleSoftwareTrainingLink: null.NewString("testing software training link"),
+				UserID:                       existingUserID,
+			},
+			wantErr: false,
+		},
 	}
 
 	// custom functions to ignore fields in cmp.Equal comparison
@@ -80,7 +121,7 @@ func TestOnboarderService_CreateOrUpdate(t *testing.T) {
 				return
 			}
 			if !cmp.Equal(got, tt.want, opts...) {
-				t.Errorf("OnboarderService.CreateOrUpdate() = %v, want %v", got, tt.want)
+				t.Errorf("OnboarderService.CreateOrUpdate().\n %v", cmp.Diff(got, tt.want, opts...))
 			}
 		})
 	}
