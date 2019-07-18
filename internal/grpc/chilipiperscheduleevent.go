d156163dcdbaff92c808ca1663825ea3a2addc74
Zach Toolson <zach.toolson@getweave.com>
2019-07-18 09:40:33 -0600

    Implement ChiliPiperScheduleEvent grpc Create endpoint

--- internal/grpc/chilipiperscheduleevent.go
+++ internal/grpc/chilipiperscheduleevent.go
@@ -44,7 +44,22 @@ func (s *ChiliPiperScheduleEventServer) ByLocationID(ctx context.Context, req *i
 }
 
 func (s *ChiliPiperScheduleEventServer) Create(ctx context.Context, req *insysproto.CreateChiliPiperScheduleEventRequest) (*insysproto.CreateChiliPiperScheduleEventResponse, error) {
-	return nil, nil
+	event, err := convertProtoToChiliPiperScheduleEvent(req)
+	if err != nil {
+		return nil, wgrpc.Error(wgrpc.CodeInvalidArgument, werror.Wrap(err, "could not convert request into internal chili piper schedule events truct").Add("req", req))
+	}
+
+	createResponse, err := s.chiliPiperScheduleEventService.Create(ctx, event)
+	if err != nil {
+		return nil, wgrpc.Error(wgrpc.CodeInternal, werror.Wrap(err, "error creating chili piper schedule event").Add("event", event))
+	}
+
+	result, err := convertChiliPiperScheduleEventToProto(createResponse)
+	if err != nil {
+		return nil, wgrpc.Error(wgrpc.CodeInternal, werror.Wrap(err, "error converting chili piper schedule event into proto").Add("createResponse", createResponse))
+	}
+
+	return result, nil
 }
 
 func convertChiliPiperScheduleEventsToProto(events []app.ChiliPiperScheduleEvent) (*insysproto.ByLocationIDChiliPiperScheduleEventResponse, error) {
@@ -58,6 +73,38 @@ func convertChiliPiperScheduleEventsToProto(events []app.ChiliPiperScheduleEvent
 	err = json.Unmarshal(eventsJSON, &result.Events)
 	if err != nil {
 		return nil, werror.Wrap(err, "could not unmarshal chili piper schedule json into proto struct")
+	}
+
+	return &result, nil
+}
+
+func convertChiliPiperScheduleEventToProto(event *app.ChiliPiperScheduleEvent) (*insysproto.CreateChiliPiperScheduleEventResponse, error) {
+	var result insysproto.CreateChiliPiperScheduleEventResponse
+
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		return nil, werror.Wrap(err, "could not marshal chili piper schedule event into json").Add("event", event)
+	}
+
+	err = json.Unmarshal(eventJSON, &result.Event)
+	if err != nil {
+		return nil, werror.Wrap(err, "could not unmarshal chili piper schedule json into proto struct").Add("eventJSON", string(eventJSON))
+	}
+
+	return &result, nil
+}
+
+func convertProtoToChiliPiperScheduleEvent(in *insysproto.CreateChiliPiperScheduleEventRequest) (*app.ChiliPiperScheduleEvent, error) {
+	var result app.ChiliPiperScheduleEvent
+
+	inJSON, err := json.Marshal(in.Event)
+	if err != nil {
+		return nil, werror.Wrap(err, "could not marshal input proto to json").Add("in", in)
+	}
+
+	err = json.Unmarshal(inJSON, &result)
+	if err != nil {
+		return nil, werror.Wrap(err, "could not unmarshal json into iternal chili piper schedule event").Add("inJSON", string(inJSON))
 	}
 
 	return &result, nil
--- internal/grpc/chilipiperscheduleevent_test.go
+++ internal/grpc/chilipiperscheduleevent_test.go
@@ -130,3 +130,98 @@ func TestChiliPiperScheduleEventServer_ByLocationID(t *testing.T) {
 		})
 	}
 }
+
+func TestChiliPiperScheduleEventServer_Create(t *testing.T) {
+	id := uuid.NewV4()
+	locationUUID := uuid.NewV4()
+	currentTime := time.Now()
+
+	successfulChiliPiperScheduleEventService := &mock.ChiliPiperScheduleEventService{
+		CreateFn: func(ctx context.Context, scheduleEvent *app.ChiliPiperScheduleEvent) (*app.ChiliPiperScheduleEvent, error) {
+			return &app.ChiliPiperScheduleEvent{
+				ID:         id,
+				LocationID: locationUUID,
+
+				AssigneeID: null.NewString("testing assignee id 1"),
+				ContactID:  null.NewString("testing contact id 1"),
+				EventID:    null.NewString("testing event id 1"),
+				EventType:  null.NewString("testing event type 1"),
+				RouteID:    null.NewString("testing route id 1"),
+
+				StartAt: null.NewTime(currentTime),
+				EndAt:   null.NewTime(currentTime),
+
+				CreatedAt: currentTime,
+				UpdatedAt: currentTime,
+			}, nil
+		},
+	}
+
+	type fields struct {
+		chiliPiperScheduleEventService app.ChiliPiperScheduleEventService
+	}
+	type args struct {
+		ctx context.Context
+		req *insysproto.CreateChiliPiperScheduleEventRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *insysproto.CreateChiliPiperScheduleEventResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name:   "successfully create a chili piper schedule event",
+			fields: fields{chiliPiperScheduleEventService: successfulChiliPiperScheduleEventService},
+			args: args{
+				context.Background(),
+				&insysproto.CreateChiliPiperScheduleEventRequest{
+					Event: &insysproto.ChiliPiperScheduleEventRecord{
+						LocationId: locationUUID.String(),
+						EventId:    "testing event id 1",
+						EventType:  "testing event type 1",
+						RouteId:    "testing route id 1",
+						AssigneeId: "testing assignee id 1",
+						ContactId:  "testing contact id 1",
+						StartAt:    currentTime.Format(time.RFC3339),
+						EndAt:      currentTime.Format(time.RFC3339),
+					},
+				},
+			},
+			want: &insysproto.CreateChiliPiperScheduleEventResponse{
+				Event: &insysproto.ChiliPiperScheduleEventRecord{
+					Id:         id.String(),
+					LocationId: locationUUID.String(),
+					EventId:    "testing event id 1",
+					EventType:  "testing event type 1",
+					RouteId:    "testing route id 1",
+					AssigneeId: "testing assignee id 1",
+					ContactId:  "testing contact id 1",
+					StartAt:    currentTime.Format(time.RFC3339),
+					EndAt:      currentTime.Format(time.RFC3339),
+					CreatedAt:  currentTime.Format(time.RFC3339Nano),
+					UpdatedAt:  currentTime.Format(time.RFC3339Nano),
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &ChiliPiperScheduleEventServer{
+				chiliPiperScheduleEventService: tt.fields.chiliPiperScheduleEventService,
+			}
+			got, err := s.Create(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ChiliPiperScheduleEventServer.Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !cmp.Equal(got, tt.want) {
+				t.Errorf("ChiliPiperScheduleEventServer.Create(). Diff:  %v", cmp.Diff(got, tt.want))
+			}
+		})
+	}
+}
