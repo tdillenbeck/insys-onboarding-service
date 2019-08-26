d1e7d9fa5eba67e6475f9829d0316cc34bf329f3
Zach Toolson <zach.toolson@getweave.com>
2019-08-26 15:45:28 -0600

    Write test for new readbysalesforceid function

--- internal/psql/onboarder_test.go
+++ internal/psql/onboarder_test.go
@@ -387,3 +387,67 @@ func TestOnboarderService_ReadByUserID(t *testing.T) {
 		})
 	}
 }
+
+func TestOnboarderService_ReadBySalesforceUserID(t *testing.T) {
+	db := initDBConnection(t)
+	clearExistingData(db)
+
+	id := uuid.NewV4()
+	existingUserID := uuid.NewV4()
+
+	query := `INSERT INTO insys_onboarding.onboarders
+		(id, user_id, schedule_customization_link, salesforce_user_id)
+		VALUES ($1, $2, 'testing existing_schedule_customization_link', 'testing salesforce_user_id')`
+	_, err := db.ExecContext(context.Background(), query, id, existingUserID)
+	if err != nil {
+		t.Fatalf("could not create onboarder: %v\n", err)
+	}
+
+	type fields struct {
+		DB *wsql.PG
+	}
+	type args struct {
+		ctx              context.Context
+		salesforceUserID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *app.Onboarder
+		wantErr bool
+	}{
+		{
+			name:   "successfully reads an existing onboarder",
+			fields: fields{DB: db},
+			args:   args{context.Background(), "testing salesforce_user_id"},
+			want: &app.Onboarder{
+				UserID:                    existingUserID,
+				SalesforceUserID:          null.NewString("testing salesforce_user_id"),
+				ScheduleCustomizationLink: null.NewString("testing existing_schedule_customization_link"),
+			},
+			wantErr: false,
+		},
+	}
+
+	// custom functions to ignore fields in cmp.Equal comparison
+	opts := []cmp.Option{
+		cmpopts.IgnoreFields(app.Onboarder{}, "ID", "CreatedAt", "UpdatedAt"),
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &OnboarderService{
+				DB: tt.fields.DB,
+			}
+			got, err := s.ReadBySalesforceUserID(tt.args.ctx, tt.args.salesforceUserID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("OnboarderService.ReadBySalesforceUserID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !cmp.Equal(got, tt.want, opts...) {
+				t.Errorf("OnboarderService.ReadBySalesforceUserID(). Diff:  %v", cmp.Diff(got, tt.want, opts...))
+			}
+		})
+	}
+}
