09ff9cb4a73e5ec822ce4a1f94a131947f80eae0
Mike Campbell <mike.campbell@getweave.com>
2019-10-08 13:46:07 -0600

    reordered fields

--- internal/app/models.go
+++ internal/app/models.go
@@ -70,11 +70,11 @@ type OnboardersLocation struct {
 	ID                      uuid.UUID
 	OnboarderID             uuid.UUID
 	LocationID              uuid.UUID
-	CreatedAt               time.Time
-	UpdatedAt               time.Time
-	UserFirstLoggedInAt     null.Time
 	Region                  string
 	SalesforceOpportunityID string
+	UserFirstLoggedInAt     null.Time
+	CreatedAt               time.Time
+	UpdatedAt               time.Time
 }
 
 // TaskInstance represent a location's status in a task (Waiting on Customer, Waiting on Weave, Completed, etc)
