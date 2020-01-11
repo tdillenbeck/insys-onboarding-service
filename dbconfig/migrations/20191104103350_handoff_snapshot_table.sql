ac661cb814532107e859fc2da560f5ebe7f8d8c9
Zach Toolson <zach.toolson@getweave.com>
2020-01-11 10:18:47 -0700

    Consolidate the handoff_snapshot_table migrations into one file.
    When creating a new database, I would be missing a few of the fields and
    we don't need the history of how to alter the table when we can just
    have a migration to create the table that we want. NOTE: I only changed
    this because my dump.sql after running migrations from scratch was
    removing a few columns.

--- dbconfig/migrations/20191104103350_handoff_snapshot_table.sql
+++ dbconfig/migrations/20191104103350_handoff_snapshot_table.sql
@@ -3,15 +3,28 @@
 CREATE TABLE insys_onboarding.handoff_snapshots (
   id uuid NOT NULL PRIMARY KEY,
   onboarders_location_id uuid NOT NULL UNIQUE,
-  csat_recipient_user_id uuid,
-  csat_sent_at timestamp with time zone,
 
-  created_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_at timestamp with time zone NOT NULL DEFAULT now()
+  billing_notes text,
+  csat_recipient_user_email text,
+  csat_sent_at timestamp with time zone,
+  customization_setup text,
+  customizations boolean,
+  disclaimer_type_sent text,
+  fax_port_submitted text,
+  handed_off_at timestamp with time zone,
+  network_decision text,
+  notes text,
+  point_of_contact_email text,
+  reason_for_purchase text,
+  router_make_and_model text,
+  router_type text,
+
+  created_at timestamp with time zone DEFAULT now() NOT NULL,
+  updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
 ALTER TABLE ONLY insys_onboarding.handoff_snapshots
-    ADD CONSTRAINT handoff_snapshots_onboarders_location_fkey FOREIGN KEY (onboarders_location_id) REFERENCES insys_onboarding.onboarders_location(id);
+  ADD CONSTRAINT handoff_snapshots_onboarders_location_fkey FOREIGN KEY (onboarders_location_id) REFERENCES insys_onboarding.onboarders_location(id);
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
