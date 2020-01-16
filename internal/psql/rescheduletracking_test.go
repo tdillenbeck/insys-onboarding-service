9085cce92816545827ccfb00d9ee5f4f58a89110
q45 <quintinsmith@icloud.com>
2020-01-16 15:23:43 -0700

    tests passing for reschedule tracking event service

--- internal/psql/rescheduletracking_test.go
+++ internal/psql/rescheduletracking_test.go
@@ -56,7 +56,7 @@ func TestRescheduleTrackingService_CreateOrUpdate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &RescheduleTrackingService{
+			s := &RescheduleTrackingEventService{
 				DB: tt.fields.DB,
 			}
 			got, err := s.CreateOrUpdate(tt.args.ctx, tt.args.locationID, tt.args.count, tt.args.eventType)
